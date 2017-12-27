3a097219004c40635e0754af9fd8f7a25b3d9eb0
Edisson Mata <edison20002503@gmail.com>
2017-12-27 02:28:35 +0100

    componente media hecho - falta la imagen

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import Media from './src/playlist/components/media';
+//  console.log("hola mundo")
+//const app = document.getElementById('app')
+//const holaMundo = <h1>Hola mundo!!</h1>
+render(<Media />, app)
---
+++ src/playlist/components/media.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './media.css'
+class Media extends Component {
+  render() {
+    return (
+      <div className="Media">
+        <div id="childMedia">
+          <img src="" alt="" id="img-com-1" />
+          <h3 className="text-comp">¿Por qué aprender React?...</h3>
+          <p className="text-comp">LeonidasEsteban</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Media;
