79d9b9008637d9f9937ccdfc1e260ea828ad6a77
Edisson Mata <edison20002503@gmail.com>
2017-12-30 12:31:20 +0100

    iconos con las listas hechas

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import Media from './media.js'
 import './playlist.css';
+import Play from '../../icons/components/play'
+import Volume from '../../icons/components/volume'
+import Pause from '../../icons/components/pause'
+import Full from '../../icons/components/full-screen'
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist
-    const playlist1 = this.props.data.categories[1].playlist
-    const playlist2 = this.props.data.categories[2].playlist
+
+function playlist(props) {
+    const playlist = props.data.categories[0].playlist
+    const playlist1 = props.data.categories[1].playlist
+    const playlist2 = props.data.categories[2].playlist
     console.log(playlist);
     return (
       <div id="playlist">
+        <div id="icon">
+          <Play color="red" size={50}/>
+          <Volume color="green" size={50} />
+          <Pause color="blue" size={50} />
+          <Full color="peru" size={50} />
+        </div>
         <div className="playlist">
-          {
-            playlist.map((items) => {
-              return <Media {...items} key={items.id} />
-            })
-          }
+            {
+              playlist.map((items) => {
+                return <Media {...items} key={items.id} />
+              })
+            }
         </div>
         <div className="playlist">
           {
@@ -34,6 +44,6 @@ class Playlist extends Component {
       </div>
     )
   }
-}
 
-export default Playlist
+
+export default playlist
