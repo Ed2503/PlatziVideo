90441c4cde9053b38353f7df1d6a235fc34b522a
Ed2503 <30421717+Ed2503@users.noreply.github.com>
2018-01-31 19:40:29 +0100

    Update index.js

--- src/entries/index.js
+++ src/entries/index.js
@@ -3,6 +3,6 @@ import ReactDOM, { render } from 'react-dom';
 import Playlist from './playlist/components/playlist';
 import data from './api.json';
 //  console.log("hola mundo")
-//const app = document.getElementById('app')
+const app = document.getElementById('home-container')
 //const holaMundo = <h1>Hola mundo!!</h1>
 render( <Playlist data={data} />, app)
